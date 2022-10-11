75f4cc84ce5c1cc01dd26051f905e62ae1917bf6
chuborekek <chuborekek@gmail.com>
2022-10-11 12:38:04 +0800

    Update all files

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useReducer } from 'react';
+import DigitButton from './components/DigitButton';
+import OperationButton from './components/OperationButton';
+import reducer from './components/reducer';
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {});
+
+  return (
+    <div className='wrapper'>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className='previous-operand'>{previousOperand} {operation}</div>
+        <div className='current-operand'>{currentOperand}</div>
+      </div>
+      <button className='span-two' onClick={()=>dispatch({type:ACTIONS.CLEAR})}>AC</button>
+      <button onClick={()=>dispatch({type:ACTIONS.DELETE_DIGIT})}>DEL</button>
+      <OperationButton operation="÷" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
+      <button className='span-two' onClick={()=>dispatch({type:ACTIONS.EVALUATE})}>=</button>
+    </div>
+    </div>
+  );
+}
+
+export default App
